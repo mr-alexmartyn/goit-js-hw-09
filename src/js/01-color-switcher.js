8417c68c813a4f728503c3312e182dfaf11a51ed
mr-alexmartyn <111350218+mr-alexmartyn@users.noreply.github.com>
2023-02-08 00:35:04 +0200

    update

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -5,7 +5,7 @@ function getRandomHexColor() {
 const buttonStart = document.querySelector('button[data-start]');
 const buttonStop = document.querySelector('button[data-stop]');
 const body = document.querySelector('body');
-
+buttonStop.disabled = true;
 let timerId = null;
 
 const backgroundSwitcher = function () {
@@ -16,9 +16,11 @@ const backgroundSwitcher = function () {
 buttonStart.addEventListener('click', () => {
   timerId = setInterval(backgroundSwitcher, 1000);
   buttonStart.disabled = true;
+  buttonStop.disabled = false;
 });
 
 buttonStop.addEventListener('click', () => {
   clearInterval(timerId);
   buttonStart.disabled = false;
+  buttonStop.disabled = true;
 });
